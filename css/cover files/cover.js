4e615d653a6ed8295c96f0a93a8dfe7efb33f318
Britt Mo <brittany.dunne@utexas.edu>
2016-11-09 11:42:56 -0600

    one-page-scroll plug-in

--- css/cover files/cover.js
+++ css/cover files/cover.js
@@ -3,24 +3,27 @@
  */
 'use strict';
 
-console.log($("#mainDiv").hide())
+
+
+$(".main").onepage_scroll({
+    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+    animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+    beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+    afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+    loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+    keyboard: true,                  // You can activate the keyboard controls
+    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+
+    direction: "vertical"
+});
+
 
 $("#introImage").animate({
-    opacity: 0.10,
-}, 3000, function() {
-    $("#mainDiv").removeClass("hidden").fadeIn(3000);
-});
-
-$('#nav').affix({
-    offset: {
-        top: $('#nav').offset().top
-    }
-});
-
-$('#nav').affix({
-    offset: {
-        bottom: ($('footer').outerHeight(true) +
-        $('.application').outerHeight(true)) +
-        40
-    }
-});
+ opacity: 0.10,
+ }, 3000, function() {
+ $("#mainDiv").removeClass("hidden").fadeIn(3000);
+ });
